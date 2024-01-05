2a754bdf0cd4b8d906716fa101d615ce302af1ca
arturbaccarin <baccarin.artur@gmail.com>
2024-01-05 17:27:38 -0300

    add questions statements

--- ch1/exercise/ex1_2.go
+++ ch1/exercise/ex1_2.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// Exercise 1.2: Modify the echo program to print the index and value of each of its arguments, one per line.
 func Exercise1_2() {
 	for idx, arg := range os.Args[1:] {
 		fmt.Println(idx, arg)
--- ch1/exercise/ex1_3_test.go
+++ ch1/exercise/ex1_3_test.go
@@ -6,6 +6,10 @@ import (
 	"github.com/arturbaccarin/the-go-programming-language-book/ch1"
 )
 
+/*
+Exercise 1.3: Experiment to measure the difference in running time between our potentially
+inefficient versions and the one that uses strings.Join.
+*/
 func BenchmarkEcho1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ch1.Echo1()
