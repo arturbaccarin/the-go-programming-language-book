57691ceabbfcf88550d5d23e04dc9df875133fed
arturbaccarin <baccarin.artur@gmail.com>
2024-01-05 17:27:00 -0300

    improve the code

--- ch1/dup2.go
+++ ch1/dup2.go
@@ -23,12 +23,14 @@ func Dup2() {
 			f.Close()
 		}
 	}
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
+
 func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
