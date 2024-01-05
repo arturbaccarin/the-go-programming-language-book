12a81cff3906c75da58d89709dbdc8e3927d22ec
arturbaccarin <baccarin.artur@gmail.com>
2024-01-05 16:27:19 -0300

    finish dup part

---
+++ ch1/dup1.go
@@ -0,0 +1,22 @@
+package ch1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Dup1() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
