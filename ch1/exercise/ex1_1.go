4b339a62e33146fc792d6080db50abc0019285b8
Artur B Baccarin <78510186+arturbaccarin@users.noreply.github.com>
2024-03-24 02:35:14 -0300

    Atualizar o ex1_1.go

--- ch1/exercise/ex1_1.go
+++ ch1/exercise/ex1_1.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-// Exercise 1.1: Modify the echo program to also print os.Args[0], the name of the command that invoked it.
+// Exercise 1.1 - Modify the echo program to also print os.Args[0], the name of the command that invoked it.
 func Exercise1_1() {
 	fmt.Println(strings.Join(os.Args[0:], " "))
 }
