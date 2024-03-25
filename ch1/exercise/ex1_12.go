3f228399113334e520b73da0e784722b08ae8668
Artur B Baccarin <78510186+arturbaccarin@users.noreply.github.com>
2024-03-25 07:01:09 -0300

    Atualizar o ex1_12.go

--- ch1/exercise/ex1_12.go
+++ ch1/exercise/ex1_12.go
@@ -15,7 +15,7 @@ import (
 )
 
 /*
-Exercise 1.12: Modify the Lissajous server to read parameter values from the URL.
+Exercise 1.12 - Modify the Lissajous server to read parameter values from the URL.
 For example, you might arrange it so that a URL like http://localhost:8000/?cycles=20 sets the
 number of cycles to 20 instead of the default 5.
 */
