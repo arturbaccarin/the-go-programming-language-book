ca1e266d88bd5328ffe210aaa8fe28e9f782f4d1
arturbaccarin <baccarin.artur@gmail.com>
2024-09-12 13:56:21 -0300

    returning with codes of go programmiong book

---
+++ ch2/cf.go
@@ -0,0 +1,23 @@
+package ch2
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+
+		f := Fahrenheit(t)
+		c := Celsius(t)
+
+		fmt.Printf("%s = %s, %s = %s\n",
+			f, FToC(f), c, CToF(c))
+	}
+}
--- ch2/tempconv.go
+++ ch2/tempconv.go
@@ -13,3 +13,7 @@ const (
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
