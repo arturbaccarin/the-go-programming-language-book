fa4e26cb7355011b225e21df2176145c0b008174
arturbaccarin <baccarin.artur@gmail.com>
2024-09-13 16:37:48 -0300

    add some codes of chapter4

---
+++ ch4/append.go
@@ -0,0 +1,22 @@
+package ch4
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		// There is room to grow. Extend the slice.
+		z = x[:zlen]
+	} else {
+		// There is insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized linear complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x) // a built-in function; see text
+	}
+
+	z[len(x)] = y
+	return z
+}
---
+++ ch4/nonempty.go
@@ -0,0 +1,13 @@
+package ch4
+
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+
+	return strings[:i]
+}
