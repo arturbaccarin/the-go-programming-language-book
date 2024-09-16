2b6981192f29b05c2c398caeb8dc6aa8f2a94edc
arturbaccarin <baccarin.artur@gmail.com>
2024-09-16 10:54:41 -0300

    add comments for nonempty code

--- ch4/nonempty.go
+++ ch4/nonempty.go
@@ -1,5 +1,7 @@
 package ch4
 
+// nonempty returns a slice holding only the non-empty strings.
+// The underlying array is modified during the call.
 func nonempty(strings []string) []string {
 	i := 0
 	for _, s := range strings {
