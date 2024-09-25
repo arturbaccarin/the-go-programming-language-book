cbf9d7970f96a04c97e796b85f87cd75762ddd25
arturbaccarin <baccarin.artur@gmail.com>
2024-09-25 07:07:36 -0300

    add more codes of chapter4

---
+++ ch4/embed.go
@@ -0,0 +1,56 @@
+package ch4
+
+import "fmt"
+
+type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}
+
+/*
+var w Wheel
+w.X = 8 // equivalent to w.Circle.Point.X = 8
+w.Y = 8 // equivalent to w.Circle.Point.Y = 8
+w.Radius = 5 // equivalent to w.Circle.Radius = 5
+w.Spokes = 20
+
+type Circle struct {
+	Center Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle Circle
+	Spokes int
+}
+*/
+
+func main() {
+	var w Wheel
+	w = Wheel{Circle{Point{8, 8}, 5}, 20}
+	w = Wheel{
+		Circle: Circle{
+			Point:  Point{X: 8, Y: 8},
+			Radius: 5,
+		},
+		Spokes: 20, // NOTE: trailing comma necessary here (and at Radius)
+	}
+
+	fmt.Printf("%#v\n", w)
+	// Output:
+	// Wheel{Circle:Circle{Point:Point{X:8, Y:8}, Radius:5}, Spokes:20}
+
+	w.X = 42
+	fmt.Printf("%#v\n", w)
+	// Output:
+	// Wheel{Circle:Circle{Point:Point{X:42, Y:8}, Radius:5}, Spokes:20}
+}
---
+++ ch4/movie.go
@@ -0,0 +1,23 @@
+package ch4
+
+/*
+Consider an application that gathers movie reviews and offers recommend ations. Its Movie
+data type and atypical list of values are declared below.  (The string literals after the Year and
+Color ﬁeld declarations are ﬁeld tags; we’ll explain them in a moment.)
+*/
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie{
+	{Title: "Casablanca", Year: 1942, Color: false,
+		Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true,
+		Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true,
+		Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+	// ...
+}
