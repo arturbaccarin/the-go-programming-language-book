c108b9875ec1286cb7e00857ac870a7931fbe25e
arturbaccarin <baccarin.artur@gmail.com>
2024-10-05 12:36:56 -0300

    add ch6 and start ch7

---
+++ ch6/geometry.go
@@ -0,0 +1,16 @@
+package ch6
+
+// Let’s write our ﬁrst method in a simple package for plane geometry:
+import "math"
+
+type Point struct{ X, Y float64 }
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// same thing, but as a method of the Point type
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
---
+++ ch7/bytecounter.go
@@ -0,0 +1,8 @@
+package ch7
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
